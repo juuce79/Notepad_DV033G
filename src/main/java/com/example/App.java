a43ab89466b83d59c023a8989ed2aa60d0949e48
Frank Lloyd Curwen <juuce79@gmail.com>
2024-11-29 12:28:43 +0100

    updated app by fixing all SonarLint issues except for 1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,59 +1,28 @@
 package com.example;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.FileNotFoundException;
-import java.io.File;
+import java.io.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.Font;
+import java.util.Arrays;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 import javax.swing.event.DocumentEvent;
-import javax.swing.JTextArea;
+import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.plaf.ScrollBarUI;
-import javax.swing.plaf.basic.BasicScrollBarUI;
-import javax.swing.plaf.ScrollPaneUI;
-import javax.swing.plaf.basic.BasicScrollPaneUI;
-import javax.swing.KeyStroke;
-import javax.swing.JFileChooser;
-import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.ScrollPaneLayout;
-import javax.swing.Scrollable;
-import javax.swing.AbstractAction;
-
-import javax.print.*;
-
 
 public class App extends JFrame implements ActionListener {
     JTextArea t;
     JFrame f;
 
     private String path = "";
-    private String default_title = "Untitled.txt";
-    private JFileChooser j;
-    private FileNameExtensionFilter filter;
+    private String defaultTitle = DEFAULT_TITLE_CONSTANT;
+    private final JFileChooser j;
+    private static final String APP_TITLE_PREFIX = "notepad - ";
+    private static final String DEFAULT_TITLE_CONSTANT = "Untitled.txt";
 
     App() {
 
@@ -63,7 +32,7 @@ public class App extends JFrame implements ActionListener {
         JMenuBar mb = new JMenuBar();
 
         JScrollPane scroll = new JScrollPane(t);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
         JMenu m1 = new JMenu("File");
         JMenu m2 = new JMenu("Edit");
@@ -143,69 +112,64 @@ public class App extends JFrame implements ActionListener {
             @Override
             public void windowClosing(WindowEvent e) {
                 if (kill() == 1) {
-                    f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                    f.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
                 }
             }
         });
 
-        t.getDocument().addDocumentListener(new javax.swing.event.DocumentListener() {
-            public void changedUpdate(javax.swing.event.DocumentEvent e) {
-                f.setTitle("notepad - " + default_title + " *");
+        t.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                f.setTitle(APP_TITLE_PREFIX + defaultTitle + " *");
             }
 
-            public void removeUpdate(javax.swing.event.DocumentEvent e) {
-                f.setTitle("notepad - " + default_title + " *");
+            public void removeUpdate(DocumentEvent e) {
+                f.setTitle(APP_TITLE_PREFIX + defaultTitle + " *");
 
             }
 
-            public void insertUpdate(javax.swing.event.DocumentEvent e) {
-                f.setTitle("notepad - " + default_title + " *");
+            public void insertUpdate(DocumentEvent e) {
+                f.setTitle(APP_TITLE_PREFIX + defaultTitle + " *");
             }
         });
 
-        t.addMouseWheelListener(new MouseWheelListener() {
-            /**
-             * @description: scroll up and down using mouse wheel
-             */
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
+        t.addMouseWheelListener(e -> {
+            // Scroll up and down using mouse wheel
+            if (e.getWheelRotation() < 0) {
+                scroll.getVerticalScrollBar().setValue(scroll.getVerticalScrollBar().getValue() - 20);
+            } else {
+                scroll.getVerticalScrollBar().setValue(scroll.getVerticalScrollBar().getValue() + 20);
+            }
+        });
+
+        t.addMouseWheelListener(e -> {
+            // Zoom in and out using mouse wheel with Alt key pressed
+            if (e.isAltDown()) {
+                int currentSize = t.getFont().getSize();
                 if (e.getWheelRotation() < 0) {
-                    scroll.getVerticalScrollBar().setValue(scroll.getVerticalScrollBar().getValue() - 20);
+                    t.setFont(new Font("Arial", Font.PLAIN, currentSize + 1));
                 } else {
-                    scroll.getVerticalScrollBar().setValue(scroll.getVerticalScrollBar().getValue() + 20);
-                }
-            }
-        });
-
-        t.addMouseWheelListener(new MouseWheelListener() {
-            /**
-             * @description: zoom in and out using mouse wheel
-             */
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if (e.isAltDown()) {
-                    if (e.getWheelRotation() < 0) {
-                        t.setFont(new Font("Arial", Font.PLAIN, t.getFont().getSize() + 1));
-                    } else {
-                        t.setFont(new Font("Arial", Font.PLAIN, t.getFont().getSize() - 1));
+                    // Ensure the font size doesn't go below a certain threshold
+                    if (currentSize > 1) {
+                        t.setFont(new Font("Arial", Font.PLAIN, currentSize - 1));
                     }
                 }
             }
         });
-        final String description[] = { "*.txt", "*.docx", "*.doc", "*.java", "*.py", "*.c", "*.cpp", "*.html", "*.css",
+
+        final String[] description = { "*.txt", "*.docx", "*.doc", "*.java", "*.py", "*.c", "*.cpp", "*.html", "*.css",
                 "*.js", "*.json", "*.xml", "*.yml", "*.yaml", "*.md", "*.markdown", "*.sql" };
-        final String extensions[] = { "txt", "docx", "doc", "java", "py", "c", "cpp", "html", "css", "js", "json",
+        final String[] extensions = { "txt", "docx", "doc", "java", "py", "c", "cpp", "html", "css", "js", "json",
                 "xml", "yml", "yaml", "md", "markdown", "sql" };
         j = new JFileChooser("C:");
         for (int i = 0; i < description.length; i++) {
-            filter = new FileNameExtensionFilter(description[i], extensions[i]);
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(description[i], extensions[i]);
             j.addChoosableFileFilter(filter);
         }
         j.setAcceptAllFileFilterUsed(false);
     }
 
     /**
-     * @param ActionEvent Action Event listner for menu items
+     * @param e Action Event listener for menu items
      * @description Listens to the menu items and performs an action based on the
      *              menu item selected
      */
@@ -227,7 +191,7 @@ public class App extends JFrame implements ActionListener {
                 break;
             case "Print":
                 try {
-                    if (path == "") {
+                    if (path.isEmpty()) {
                         JOptionPane.showMessageDialog(f, "Please save or open a file first");
                         return;
                     }
@@ -238,9 +202,9 @@ public class App extends JFrame implements ActionListener {
                         JOptionPane.showMessageDialog(f, "Printing cancelled");
                     }
                 }  catch (Exception evt) {
-                        JOptionPane.showMessageDialog(f, "File name cannot be the same" + evt.getStackTrace());
-                    break; 
-                } 
+                        JOptionPane.showMessageDialog(f, "File name cannot be the same" + Arrays.toString(evt.getStackTrace()));
+                    break;
+                }
                 break;
             case "cut":
                 t.cut();
@@ -254,6 +218,8 @@ public class App extends JFrame implements ActionListener {
             case "close":
                 kill();
                 break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + s);
         }
 
     }
@@ -261,12 +227,12 @@ public class App extends JFrame implements ActionListener {
     public void newFile() {
         // check if file contains a * which means it has been edited
         if (f.getTitle().contains("*")) {
-            int a = JOptionPane.showConfirmDialog(f, "Do you want to save changes to " + default_title + "?");
+            int a = JOptionPane.showConfirmDialog(f, "Do you want to save changes to " + defaultTitle + "?");
             if (a == JOptionPane.YES_OPTION) {
                 saveToPath();
                 t.setText("");
-                default_title = "Untitled.txt";
-                f.setTitle("notepad - " + default_title);
+                defaultTitle = DEFAULT_TITLE_CONSTANT;
+                f.setTitle(APP_TITLE_PREFIX + defaultTitle);
                 path = "";
                 return;
 
@@ -274,100 +240,91 @@ public class App extends JFrame implements ActionListener {
             if (a == JOptionPane.NO_OPTION) {
                 path = "";
                 t.setText("");
-                default_title = "Untitled.txt";
-                f.setTitle("notepad - " + default_title);
-                return;
-            } else {
-                return;
+                defaultTitle = DEFAULT_TITLE_CONSTANT;
+                f.setTitle(APP_TITLE_PREFIX + defaultTitle);
             }
         // if not saved, means file was just opened
         } else {
             t.setText("");
-            default_title = "Untitled.txt";
-            f.setTitle("notepad - " + default_title);
+            defaultTitle = DEFAULT_TITLE_CONSTANT;
+            f.setTitle(APP_TITLE_PREFIX + defaultTitle);
             path = "";
         }
 
     }
     /**
-     * @description saves a file relative to the path or calls save() if path is not defined 
+     * @description saves a file relative to the path or calls save() if path is not defined
      */
     public void saveToPath() {
-        if (path == "") {
+        if (path.isEmpty()) {
             save();
             return;
         }
-        try {
-            FileWriter fw = new FileWriter(path);
-            BufferedWriter bw = new BufferedWriter(fw);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
             t.write(bw);
-            bw.close();
-            f.setTitle("notepad - " + default_title);
-        } catch (Exception evt) {
-            JOptionPane.showMessageDialog(f, "Saving error" + evt.getMessage());
+            f.setTitle(APP_TITLE_PREFIX + defaultTitle);
+        } catch (IOException evt) {
+            JOptionPane.showMessageDialog(f, "Saving error: " + evt.getMessage());
         }
     }
 
     /**
      * @description Saves the file to specified path and returns the path (acts as a
-     *              saveas function basically)
-     * @return String path of the file
+     *              save as function basically)
      */
     public void save() {
         int r = j.showSaveDialog(null);
 
         if (r == JFileChooser.APPROVE_OPTION) {
             File fi = new File(j.getSelectedFile().getAbsolutePath());
-            try {
-                FileWriter wr = new FileWriter(fi, false);
-                BufferedWriter w = new BufferedWriter(wr);
-
-                w.write(t.getText());
-                w.flush();
-                w.close();
-                default_title = fi.getName();
-                f.setTitle("notepad" + " - " + default_title);
+            try (BufferedWriter w = new BufferedWriter(new FileWriter(fi, false))) {
+                t.write(w);
+                // No need to explicitly flush and close; try-with-resources handles it
+                defaultTitle = fi.getName();
+                f.setTitle(APP_TITLE_PREFIX + defaultTitle);
                 path = fi.getAbsolutePath();
-            } catch (Exception evt) {
-                JOptionPane.showMessageDialog(f, "Saving error" + evt.getMessage());
+            } catch (IOException evt) {
+                JOptionPane.showMessageDialog(f, "Saving error: " + evt.getMessage());
             }
-        } else
+        } else {
             JOptionPane.showMessageDialog(f, "Cancelled saving");
+        }
     }
 
     /**
      * @description Opens a file from the specified path and returns the path
-     * @return String path of the file
      */
-    private String open() {
+    private void open() {
         int r = j.showOpenDialog(null);
-        File fi = null;
+        File fi;
         if (r == JFileChooser.APPROVE_OPTION) {
             fi = new File(j.getSelectedFile().getAbsolutePath());
-            try {
-                String s1 = "", sl = "";
-                FileReader fr = new FileReader(fi);
-                BufferedReader br = new BufferedReader(fr);
-                sl = br.readLine();
-                while ((s1 = br.readLine()) != null) {
-                    sl = sl + "\n" + s1;
+            StringBuilder sl = new StringBuilder();
+            try (BufferedReader br = new BufferedReader(new FileReader(fi))) {
+                String s1;
+
+                // Read the first line
+                if ((s1 = br.readLine()) != null) {
+                    sl.append(s1);
                 }
-                t.setText(sl);
-                default_title = fi.getName();
-                f.setTitle("notepad" + " - " + default_title);
+
+                // Read the remaining lines
+                while ((s1 = br.readLine()) != null) {
+                    sl.append("\n").append(s1);
+                }
+
+                t.setText(sl.toString());
+                defaultTitle = fi.getName();
+                f.setTitle(APP_TITLE_PREFIX + defaultTitle);
                 path = fi.getAbsolutePath();
-            } catch (Exception evt) {
-                JOptionPane.showMessageDialog(f, evt.getMessage());
+            } catch (IOException evt) {
+                JOptionPane.showMessageDialog(f, "Error opening file: " + evt.getMessage());
             }
         }
-        if (fi != null) {
-            return fi.getAbsolutePath();
-        }
-        return "";
     }
 
     /**
-     * @description asks the user if wants to save the file before closing
+     * @description asks the user if it wants to save the file before closing
      *              application
      * @return int 1 if the user wants to continue editing (cancel)
      */
@@ -386,7 +343,7 @@ public class App extends JFrame implements ActionListener {
         return 1;
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         launch();
     }
 
